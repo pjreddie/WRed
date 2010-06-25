9b467716db06510d1750fdb8aab8809800095ac2
Ophir Lifshitz <ophir@P632143.(none)>
2010-06-25 10:13:57 -0400

    Fixing toolbar and panels

--- media/JavaScript/view_file.js
+++ media/JavaScript/view_file.js
@@ -14,16 +14,17 @@ Ext.onReady(function() {
     var dataArray = [];
 
     // Initialize grid for file view
-    var grid = new Ext.grid.GridPanel({
+    var FileTab = new Ext.grid.GridPanel({
+//      title:          'File Tab',
         store:          store,
         columns:        gridColumns,
         stripeRows:     true,
-        height:         300,
+        height:         600,
         autoWidth:      true,
         title:          'File Data',
         horizontalScroll: true,
         
-        id:             'GridPanel',
+        id:             'FileTab',
     });
 
 // [ CHART PANEL ]
@@ -38,7 +39,7 @@ Ext.onReady(function() {
         triggerAction:  'all',
         emptyText:      'Select X axis...',
         selectOnFocus:  true,
-        listeners:      {select: {fn: selection}},
+        listeners:      { select: { fn: selection } },
         
         id:             'xTitle',
         listClass:      'xChoiceList', /* Apply class to dropdown menu list */
@@ -52,7 +53,7 @@ Ext.onReady(function() {
         triggerAction:  'all',
         emptyText:      'Select Y axis...',
         selectOnFocus:  true,
-        listeners:      {select: {fn: selection}},
+        listeners:      { select: { fn: selection} },
         
         id:             'yTitle',
         listClass:      'yChoiceList', /* Apply class to dropdown menu list */
@@ -63,13 +64,13 @@ Ext.onReady(function() {
      // width:          500,
         id:             'xChoiceContainer',
         cls:            'ChoiceContainer',
-        items:          [xChoice],
+        items:          [ xChoice ],
     });
     var yChoiceContainer = new Ext.Container({
      // height:         500,
         id:             'yChoiceContainer',
         cls:            'ChoiceContainer',
-        items:          [yChoice],
+        items:          [ yChoice ],
     });
     
     
@@ -82,17 +83,17 @@ Ext.onReady(function() {
      // height:         500,
         valueField:     'id',
         displayField:   'name',
-     // width:          500,
+        autoWidth: true,
         
         id:             'ChartContainer',
-        items:          ['PlotContainer'],
+        items:          [ PlotContainer ],
     });
       
     
 
     var FunctionSelectStore = new Ext.data.ArrayStore({
-        data:           [ [1, 'Gaussian'], [2, '...'] ],
-        fields:         ['id', 'name'],
+        data:           [ [ 1, 'Gaussian' ], [ 2, '...' ] ],
+        fields:         [ 'id', 'name' ],
     });
     var FunctionSelect = new Ext.form.ComboBox({
         fieldLabel:     'Function',
@@ -119,8 +120,10 @@ Ext.onReady(function() {
         id:             'FitCurrentGroupButton',
     });
     var ClearCurrentCurvesButton = new Ext.Button({
-        text:           'Clear ',
+        text:           'Clear current curves',
+        handler:        function(button, event) { alert(button); },
     
+        id:             'ClearCurrentCurvesButton',
     });
         
     var FittingPanel = new Ext.FormPanel({
@@ -130,8 +133,13 @@ Ext.onReady(function() {
         defaultType:    'textfield',
         labelWidth:     80,
         
+        autoWidth:      true,
+        autoHeight:     true,
+        bodyStyle:      'padding: 10px;',
+
         id:             'FittingPanel',
-        items:          [FunctionSelect, FitCurrentGroupButton, ClearCurrentCurvesButton],
+        items:          [ FunctionSelect, FitCurrentGroupButton, ClearCurrentCurvesButton ],
+        tools:          [ { id: 'gear' }, { id: 'help' }, ],
     });
 
 
@@ -143,46 +151,46 @@ Ext.onReady(function() {
 // [ TAB PANELS ]
 
     var ToolsPanel = new Ext.Panel({
-        autoWidth:      true,
-        autoHeight:     true,
+        title:          'Tools',
+        region:         'east',
+        collapsible:    true,
+        
+        minWidth:       200,
+        layout:         'accordion',
+        layoutConfig:   { animate: true, },
         
         id:             'ToolsPanel',
-        items:          [FittingPanel],
+        items:          [ FittingPanel ],
     });
-
     
     /* Holds the chart and the two combo boxes */
     var ChartPanel = new Ext.Panel({
-        autoWidth:      true,
-        autoHeight:     true,
+        title:          'Chart',
+        region:         'center',
+        
+        minWidth:       755,
+        width:          900,
         
         id:             'ChartPanel',
-        items:          [yChoiceContainer, ChartContainer, xChoiceContainer], // Originally [xChoice, yChoice, ChartContainer], but this order is better
+        items:          [ yChoiceContainer, ChartContainer, xChoiceContainer ], // Originally [xChoice, yChoice, ChartContainer], but this order is better
+        tools:          [{
+                            id: 'refresh',
+                            qtip: 'Refresh chart',
+                            handler: activateChart,
+                        }],
     });
     
     /* Holds the chart container and the other tool panels */
     var ChartTab = new Ext.Panel({
 //      title:          'Chart Tab',
-        autoWidth:      true,
-//      autoHeight:     true,      
-        height:         600, // why not auto!?
+//      autoWidth:      true,
+        height:         588, // why not auto!?
         
         layout:         'border',
-        defaults:       { split: true, },
+        defaults:       { split: true },
         
         id:             'ChartTabPanel',
-        items:          [{
-                            title: 'Chart',
-                            region: 'center',
-                            items: [ChartPanel],
-                        },
-                        {
-                            title: 'Tools',
-                            region: 'east',
-                            width: 300,
-                            collapsible: true,
-                            items: [ToolsPanel],
-                        }],
+        items:          [ ChartPanel, ToolsPanel ],
     });
     
     
@@ -191,26 +199,25 @@ Ext.onReady(function() {
     var tabs = new Ext.TabPanel({
        renderTo:        'tabs',
        activeTab:       0, // CHANGE BACK TO 0
-       defaults:        {autoHeight: true, autoWidth: true},
+       defaults:        { autoWidth: true, autoHeight: true },
        layoutOnTabChange: true,
     });
     tabs.add({
         id:             'FileTab',
         title:          'View File',
-        items:          [grid],
+        items:          [ FileTab ],
     }).show();
     tabs.add({
-        listeners:      {activate: activateChart},
+        listeners:      { activate: activateChart },
         id:             'ChartTab',
         title:          'View Chart',
-        items:          [ChartTab],
+        items:          [ ChartTab ],
     }).show();
     
 
 
-
     /* Draws the chart when the user activates the chart tab. If no choice is specified for the graph, it defaults to A4 and Detector */
-    function activateChart(tab) {
+    function activateChart() {
         if (xChoice.getValue() == '' || yChoice.getValue == '') {
             xChoice.setValue('A4');
             yChoice.setValue('Detector');
@@ -268,7 +275,7 @@ Ext.onReady(function() {
 
         store.loadData(dataArray);
         colModel = new Ext.grid.ColumnModel({columns: gridColumns});
-        grid.reconfigure(store, colModel);
+        FileTab.reconfigure(store, colModel);
 
         if (tabs.getActiveTab().getId() == 'chart') {
             activateChart(tabs.getActiveTab());
@@ -303,7 +310,6 @@ Ext.onReady(function() {
 function drawChart(store, xChoice, yChoice, chart) {
     var chartInfo = getData(store, xChoice, yChoice);
 
-
     var plotContainer = $('#' + chart);
 
     var datapoints = {
@@ -324,8 +330,8 @@ function drawChart(store, xChoice, yChoice, chart) {
       pan: { // plugin
         interactive: true
       },
-      grid: {hoverable: true, clickable: true},
-      //yaxis: {autoscaleMargin: null},
+      grid: { hoverable: true, clickable: true },
+      //yaxis: { autoscaleMargin: null },
     };
 
 
@@ -335,10 +341,10 @@ function drawChart(store, xChoice, yChoice, chart) {
         label:    xChoice + ' vs. ' + yChoice + ': Series 1',
         data:     chartInfo,
         points:   datapoints,
-        lines:    {show: false}
+        lines:    { show: false }
       }],
       options); //.addRose(); // Compass rose for panning
-    
+
 }
 
 /* Gets data from the Store to draw the chart */
